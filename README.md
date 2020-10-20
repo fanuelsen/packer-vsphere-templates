8d9601bd3cb2384c8fed12cf07902da5d5f38edc
fanuelsen@mac.com <fanuelsen@mac.com>
2020-10-20 11:03:13 +0200

    changes

--- README.md
+++ README.md
@@ -6,6 +6,16 @@
 **Step1: Prepare the environemnt**
 Populate variables.json and run environment.sh to generate newest isos url and chksum. (Windows chksum takes some time)
 
+**Variables that needs to be changed:**
+| variables.json | 
+|-|
+|```"vcenter_server":"0.0.0.0",```|
+|```"vcenter_username":"administrator@vsphere.local",```|
+|```"vcenter_password":"password",``` |
+|```"vsphere_cluster": "vspherecluser",```|
+|```"vsphere_datacenter": "vspherepool",```|
+|```"vsphere_datastore": "vspheredatastore",```|
+
 **Step2: Build the template**
 To create the template execute **packer build -force -var-file variables.json -var-file isovars.json ./linux/ubuntu/ubuntu1804.json** 
 
--- environment.sh
+++ environment.sh
@@ -1,26 +1,6 @@
-echo "==> Getting latest ubuntu iso url"
-url_1804=http://cdimage.ubuntu.com/releases/18.04/release/
-iso_1804=$(curl "$url_1804" | grep server-amd64.iso | sed -E 's/^.+href="([^"]+)".+$/\1/' | head -1)
-sha256_1804=$(curl "$url_1804/SHA256SUMS" | grep server-amd64.iso | cut -f 1 -d ' ')
-
-url_2004=http://cdimage.ubuntu.com/ubuntu-legacy-server/releases/20.04/release/
-iso_2004=$(curl "$url_2004" | grep server-amd64.iso | sed -E  's/^.+href="([^"]+)".+$/\1/' | head -1 )
-sha256_2004=$(curl "$url_2004/SHA256SUMS" | grep server-amd64.iso | sort -r | head -1 | cut -f 1 -d ' ')
-
-echo "==> Getting latest windows iso url"
-url_win2019=https://www.microsoft.com/en-US/evalcenter/evaluate-windows-server-2019?filetype=ISO
-iso_win2019=$(curl "$url_win2019" | grep .iso | grep en-us | sed -ne 's/.*\(https[^"]*\).*/\1/p' | sed 's/&quot;//g' | tr -d "\r" )
-sha256_win2019=$(curl -s "$iso_win2019" | openssl sha256)
-
-echo "==> Populate iso variable"
-cat > isovars.json <<EOF
-{
-  "iso_url_1804": "$url_1804$iso_1804",
-  "iso_checksum_1804": "$sha256_1804",
-  "iso_url_2004": "$url_2004$iso_2004",
-  "iso_checksum_2004": "$sha256_2004",
-  "iso_url_win2019": "$iso_win2019",
-  "iso_checksum_win2019": "$sha256_win2019",
-  "iso_checksum_type": "sha256"
-}
-EOF
+export VCENTER_SERVER=
+export VCENTER_USERNAME=
+export VCENTER_PASSWORD=
+export VSPHERE_DATACENTER=
+export VSPHERE_CLUSTER=
+export VSPHERE_DATASTORE=
