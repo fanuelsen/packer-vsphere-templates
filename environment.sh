cfa20f0ddb51ef765615fbf816a309e33b3ad1eb
fanuelsen@mac.com <fanuelsen@mac.com>
2020-10-20 12:57:40 +0200

    fixed vars, added support for drone

--- environment.sh
+++ environment.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+echo "==> Setting environment variables"
 export VCENTER_SERVER=
 export VCENTER_USERNAME=
 export VCENTER_PASSWORD=
 export VSPHERE_DATACENTER=
 export VSPHERE_CLUSTER=
 export VSPHERE_DATASTORE=
+export VSPHERE_NETWORK=
