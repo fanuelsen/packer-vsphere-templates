bb9d0979a91351a6c0718644cf4755d759da2dd4
fanuelsen@mac.com <fanuelsen@mac.com>
2020-11-04 09:11:36 +0100

    moved winrm basic to vmware runonce

--- scripts/enablewinrmbasic.ps1
+++
@@ -1,3 +0,0 @@
-# When NOT USING a domain-based authentication, remember to disable this after VM provisoning!
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
